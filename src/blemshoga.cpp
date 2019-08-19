1ede9c335d260fc1af1eddd7c303fb0e80cb88ec
Jacek Wieczorek <mrjjot@gmail.com>
2019-08-19 17:11:26 +0200

    Ok it works now

--- src/blemshoga.cpp
+++ src/blemshoga.cpp
@@ -4,7 +4,25 @@
 int main( )
 {
 	std::cerr << "blemshoga is up!" << std::endl;
+	
+	sf::Window window( sf::VideoMode( 800, 600 ), "bingo bango bongo blemshoga" );
 
+	while ( window.isOpen( ) )
+	{
+		sf::Event ev;
+		while ( window.pollEvent( ev ) )
+		{
+			switch ( ev.type )
+			{
+				case sf::Event::Closed:
+					window.close( );
+					break;
+
+				default:
+					break;
+			}
+		}
+	}
 
 	return 0;
 }
